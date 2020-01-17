c8645a98949e4fd32036199f68f77904a9d37286
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-01-17 11:48:23 -0300

    Interface mobile feita em React Native para visualização de devs no mapa

---
+++ src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+function Profile({ navigation }) {
+  const githubusername = navigation.getParam('github_username');
+  return <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubusername}` }} />
+}
+
+export default Profile;
